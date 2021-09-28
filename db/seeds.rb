7637880bf9e058f5f3d870bd39618216f28a3f35
WalukaUI <cwaluka@yahoo.com>
2021-09-28 13:18:57 -0700

    patient table seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding data..."
+
 Patient.destroy_all
 Location.destroy_all
 Doctor.destroy_all
@@ -12,8 +15,18 @@ DoctorLocation.destroy_all
 Appointment.destroy_all
 Comment.destroy_all
 
-100.times do
+
+10.times do
     patient=Patient.create!(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        contact_number: Faker::PhoneNumber.cell_phone,
+        clinic_location: Faker::Address.community,
+        role: "patient",
         username: Faker::Internet.username,
+        password_digest: Faker::Address.country
     )
 end
+
+puts "✅ Done seeding!"

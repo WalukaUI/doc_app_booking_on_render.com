3a211d529703bef5703592769827e9bd5faa26c6
WalukaUI <cwaluka@yahoo.com>
2021-09-29 00:10:31 -0700

    added custom routes to routes.rb and created func in patientcontroller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,36 @@
 class PatientsController < ApplicationController
+    # before_action :authorize, only: [:show]
+
+    def index
+     patient=Patient.all
+     render json: patient.to_json(except: [:created_at, :updated_at, :username, :password_digest])
+    end
+    
+    def create
+     patient = Patient.create(patient_params)
+    if patient.valid?
+    #   session[:patient_id] = patient.id
+      render json: patient, status: :created
+    else
+      render json: { error: patient.errors.full_messages }, status: :unprocessable_entity
+    end
+    end
+    def destroy
+      patient = Patient.destroy(params[:id])
+      render json: {message: "Patient deleted"}
+    end
+    def show
+    #   patient = Patient.find_by(id: session[:patient_id])
+      patient = Patient.find_by(id: params[:id])
+      render json: patient.to_json(except: [:created_at, :updated_at, :username, :password_digest], include: [comment: {except: [:created_at, :updated_at]}]) 
+    end
+  
+    private
+    # def authorize
+    #   return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :patient_id
+    # end
+
+    def patient_params
+      params.permit(:first_name, :last_name, :username, :email, :contact_number, :clinic_location, :role, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   resources :doctor_locations, only: [:show, :index]
   resources :doctors
   resources :locations
-  resources :patients
+  resources :patients 
+
+  post "/signup", to: "patient#create"
+  get "/me", to: "patient#show"
+  post "/patientlogin", to: "sessions#createpatient"
+  post "/doctorlogin", to: "sessions#createdoctor"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

2154299837459e50ed342808213ce1d20845c6dc
WalukaUI <cwaluka@yahoo.com>
2021-09-29 20:00:43 -0700

    locaton controller index func

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,11 @@
 class LocationsController < ApplicationController
+    def index
+        if params[:doctor_id]
+          doctor = Doctor.find(params[:doctor_id])
+          location = doctor.location
+        else
+          project = Location.all
+        end
+        render json: project
+      end
 end

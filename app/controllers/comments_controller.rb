c2f956dd5676aef6969c08db9ee3245bdc2237e2
WalukaUI <cwaluka@yahoo.com>
2021-09-29 23:33:58 -0700

    comment methords added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,44 @@
 class CommentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    def index
+        comment = Comment.all
+        render json: comment.to_json(except: [:created_at, :updated_at])
+      end
+
+      def show
+        comment = Comment.find(params[:id])
+        render json: comment.to_json(except: [:created_at, :updated_at]), status: 200
+      end
+
+      def create
+        comment = Comment.create(comment_params)
+        render json: comment.to_json(except: [:created_at, :updated_at]), status: 201
+      end
+
+      def update
+        comment = Comment.find(params[:id])
+        comment.update!(comment_params)
+        render json: comment.to_json(except: [:created_at, :updated_at])
+      end
+
+      def destroy
+        comment = Comment.destroy(params[:id])
+        render json: {message: "Comment deleted"}
+      end
+
+      private
+
+      def comment_params
+        params.permit(:name, :address_line_one, :address_line_two, :city, :zipcode, :contact_number, :latitude, :longitude)
+      end
+
+      def render_not_found
+        render json: { error: "Comment is not found" }, status: :not_found
+      end
+
+      def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
 end

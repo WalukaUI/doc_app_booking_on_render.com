1dd6154ce33371f1b486c1ade52da03e403a6984
WalukaUI <cwaluka@yahoo.com>
2021-09-29 19:44:13 -0700

    patient CRUD created

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,6 @@
 class PatientsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     # before_action :authorize, only: [:show]
 
     def index
@@ -15,10 +17,18 @@ class PatientsController < ApplicationController
       render json: { error: patient.errors.full_messages }, status: :unprocessable_entity
     end
     end
+
+    def update
+      patient = Patient.find(params[:id])
+      patient.update!(patient_params)
+      render json: patient.to_json(except: [:created_at, :updated_at, :username, :password_digest])
+    end
+
     def destroy
       patient = Patient.destroy(params[:id])
       render json: {message: "Patient deleted"}
     end
+
     def show
     #   patient = Patient.find_by(id: session[:patient_id])
       patient = Patient.find_by(id: params[:id])
@@ -31,6 +41,14 @@ class PatientsController < ApplicationController
     # end
 
     def patient_params
-      params.permit(:first_name, :last_name, :username, :email, :contact_number, :clinic_location, :role, :password, :password_confirmation)
+      params.permit(:first_name, :last_name, :username, :email, :contact_number, :clinic_location, :role, :password_digest, :password_confirmation)
+    end
+
+    def render_not_found
+      render json: { error: "Patient is not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
     end
 end

d178bd881531038fb5ef55ca47f9a0c6394f5c70
WalukaUI <cwaluka@yahoo.com>
2021-10-02 23:57:14 -0700

    added middleware/securecookies.rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require_relative "../lib/middleware/secure_cookies"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -36,5 +37,9 @@ module DoctorBookingManager
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.insert_after ActionDispatch::Static, Middleware::SecureCookies
   end
 end

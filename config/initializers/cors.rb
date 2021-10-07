d0307b13bec97c5012bc3653c3d9af53bf60eee8
WalukaUI <cwaluka@yahoo.com>
2021-10-07 13:39:43 -0700

    cors.rb set origin to *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'https://doctor-appointment-creator.netlify.app' : 'http://localhost:3001'
+    # origins Rails.env.production? ? 'https://doctor-appointment-creator.netlify.app' : 'http://localhost:3001'
+    origins '*'
 
     resource '*',
       headers: :any,

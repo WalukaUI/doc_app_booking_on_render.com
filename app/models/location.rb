dd11b4793b59dd78d28b9d00d7b333e0fb379b6f
WalukaUI <cwaluka@yahoo.com>
2021-10-07 12:30:53 -0700

    2nd version to heroku

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,6 @@ class Location < ApplicationRecord
     has_many :doctor_locations
     has_many :doctors, through: :doctor_locations
 
-    validates :name, uniqueness: true
     validates :name, presence: true 
     validates :longitude, presence: true 
     validates :latitude, presence: true  

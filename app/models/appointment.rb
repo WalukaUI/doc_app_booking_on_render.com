6491ef6bb3c013397853f348c73dafd0d9aff830
WalukaUI <cwaluka@yahoo.com>
2021-11-30 00:44:43 -0800

    validation added to new appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
 
- # validates :date, presence: true
+  validates :date, presence: true
   validates :doctor_id, presence: true
+  validates :time, presence: true
 end

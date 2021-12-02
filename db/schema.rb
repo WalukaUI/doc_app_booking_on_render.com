91de5c1847add6bb9ec595ebe6e665a7583e0fbc
WalukaUI <cwaluka@yahoo.com>
2021-12-02 12:46:33 -0800

    change points column to data type flote in comments tbl

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_041243) do
+ActiveRecord::Schema.define(version: 2021_12_02_204417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_10_12_041243) do
     t.bigint "doctor_id"
     t.bigint "patient_id"
     t.string "comment"
-    t.integer "points"
+    t.float "points"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["doctor_id"], name: "index_comments_on_doctor_id"
